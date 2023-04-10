127dc8c68f0b10c9850fb5e2611a6e738d0f2cee
Jon <jon.borgenjr@tanduo.io>
2023-04-09 21:08:59 -0500

    stubbed out api call to arkham db

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,24 @@
 use bevy::prelude::*;
+//use serde::Deserialize;
+use reqwest::Error;
 
 // Bevy Doc
 // https://bevyengine.org/learn/book/getting-started/apps/
 fn main() {
     println!("Game Started!");
+    // WIP Update function
+    //update_assets();
     App::new().run();
+}
+
+#[tokio::main]
+async fn update_assets() -> Result<(), Error> {
+    println!("Updating Assets!");   
+    // Gets all player cards, to get encounter cards, set encounter parameter to 1
+    let request_url = "https://arkhamdb.com/api/public/cards/";
+    println!("{}", request_url);
+
+    let response = reqwest::get(&request_url).await?;
+    println!("{}", response);
+    Ok(())
 }
