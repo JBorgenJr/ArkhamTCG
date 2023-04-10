45abf87fe01d3fc182bf4f565578647fa9879a7a
Jon <jon.borgenjr@tanduo.io>
2023-04-10 17:28:06 -0500

    Added arguments for triggering fetch Uploading all cards in json format

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,44 @@
-use bevy::prelude::*;
-//use serde::Deserialize;
-use reqwest::Error;
+//use bevy::prelude::*;
+use std::fs::File;
+use std::io::Write;
 
 // Bevy Doc
 // https://bevyengine.org/learn/book/getting-started/apps/
-fn main() {
+#[tokio::main]
+async fn main() {
     println!("Game Started!");
-    // WIP Update function
-    //update_assets();
-    App::new().run();
+
+    let args: Vec<String> = std::env::args().collect();
+
+    // Check for update argument and trigger fetch of card list
+    if args.contains(&"-update".to_string()) {
+        fetch_cards().await;
+    }
+
+    //App::new().run();
 }
 
-#[tokio::main]
-async fn update_assets() -> Result<(), Error> {
-    println!("Updating Assets!");   
-    // Gets all player cards, to get encounter cards, set encounter parameter to 1
-    let request_url = "https://arkhamdb.com/api/public/cards/";
-    println!("{}", request_url);
+async fn fetch_cards() {
+    // Retrieve latest card list from ArkhamDB public API
+    let response = reqwest::get("https://arkhamdb.com/api/public/cards/?encounter=1")
+        .await
+        .unwrap();
 
-    let response = reqwest::get(&request_url).await?;
-    println!("{}", response);
-    Ok(())
+    let status = response.status();
+
+    match status {
+        reqwest::StatusCode::OK => {
+            let json_string = response.text().await.unwrap();
+
+            // Write JSON to file
+            let mut file = File::create("cards.json").unwrap();
+            file.write_all(json_string.as_bytes()).unwrap();
+        }
+        reqwest::StatusCode::UNAUTHORIZED => {
+            println!("Unable to fetch cards, unauthorized");
+        }
+        _ => {
+            panic!("Unable to fetch cards, something went wrong");
+        }
+    };
 }
