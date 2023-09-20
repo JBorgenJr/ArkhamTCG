e262442915e068ff382daee4997d763658b8dacd
Jon <jon.borgenjr@tanduo.io>
2023-09-20 15:59:06 -0500

    Improved API calls Saved player and encounter data

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use reqwest::Result;
-use std::env;
+use reqwest::{Response, Result};
+use serde_json::{from_str, Value};
+use std::{env, fs};
 
+const CARDS_JSON: &str = "assets/cards/cards.json";
 const BASE_URL: &str = "https://arkhamdb.com/api/public/";
 
 #[tokio::main]
@@ -17,26 +19,17 @@ async fn main() {
 
     // check which command the user provided
     match args[1].as_str() {
-        // if the user provided the "get-card" command
-        "get-card" if args.len() == 3 => {
-            let argument = &args[2];
-            if let Err(err) = fetch_data(format!("{}card/{}", BASE_URL, argument).as_str()).await {
+        // if the user provided the "update-player-cards" command
+        "update-player-cards" if args.len() == 2 => {
+            if let Err(err) = fetch_data(format!("{}cards/", BASE_URL).as_str()).await {
                 eprintln!("Error: {:?}", err);
             }
         }
-        // if the user provided the "get-pack" command
-        "get-pack" if args.len() == 3 => {
-            let argument = &args[2];
-            if let Err(err) = fetch_data(format!("{}cards/{}", BASE_URL, argument).as_str()).await {
+        // if the user provided the "update-all-cards" command
+        "update-all-cards" if args.len() == 2 => {
+            if let Err(err) = fetch_data(format!("{}cards/?encounter=1", BASE_URL).as_str()).await {
                 eprintln!("Error: {:?}", err);
             }
-        }
-        // if the user provided the "get-all-cards" command
-        "get-all-cards" if args.len() == 2 => {
-            // if let Err(err) = fetch_data(format!("{}/cards", BASE_URL).as_str()).await {
-            //     eprintln!("Error: {:?}", err);
-            // }
-            eprintln!("Not implemented yet.");
         }
         _ => {
             // if the user provided an unknown command
@@ -48,19 +41,92 @@ async fn main() {
 
 // fetch data from the API
 async fn fetch_data(url: &str) -> Result<()> {
-    let response = reqwest::get(url).await?;
-    let response_bytes = response.bytes().await?;
-    let body = String::from_utf8(response_bytes.to_vec()).unwrap();
+    let response: Response = reqwest::get(url).await?;
 
-    println!("API response: {:?}", body);
+    // check if the request was successful
+    if !response.status().is_success() {
+        eprintln!("Error: {:?}", response.status());
+        return Ok(());
+    }
+
+    // deserialize the response into a JSON object
+    let response_json: Value = response.json().await?;
+
+    update_cards(Some(response_json));
 
     Ok(())
+}
+
+fn update_cards(cards: Option<Value>) {
+    println!("Updating cards...");
+
+    // check if cards was provided
+    if let Some(cards) = cards {
+        println!("Updating card data...");
+        // create cards.json file in assets folder from cards variable
+        let cards_json = serde_json::to_string_pretty(&cards).unwrap();
+        fs::write(CARDS_JSON, cards_json).unwrap();
+    }
+
+    // create a new directory for the cards in assets folder if it doesnt exist
+    fs::create_dir_all("assets/cards/investigators").unwrap();
+    fs::create_dir_all("assets/cards/encounters").unwrap();
+
+    // get the cards.json file
+    let cards_json = fs::read_to_string(CARDS_JSON).unwrap();
+
+    // parse the cards.json file into a JSON object
+    let cards_json: Value = from_str(&cards_json).unwrap();
+
+    // seperate by card types
+    let (investigator_cards, encounter_cards): (Vec<&Value>, Vec<&Value>) = cards_json
+        .as_array()
+        .unwrap()
+        .iter()
+        .partition(|card| card["type_code"].as_str().unwrap() == "investigator");
+
+    // seperate encounter cards by unique pack_code
+    let mut encounter_cards_by_pack: Vec<Vec<&Value>> = Vec::new();
+    for card in encounter_cards.iter() {
+        let pack_code = card["pack_code"].as_str().unwrap();
+        let mut pack_found = false;
+        for pack in encounter_cards_by_pack.iter_mut() {
+            if pack[0]["pack_code"].as_str().unwrap() == pack_code {
+                pack.push(card);
+                pack_found = true;
+                break;
+            }
+        }
+        if !pack_found {
+            encounter_cards_by_pack.push(vec![card]);
+        }
+    }
+
+    // save investigator_cards to file
+    println!("Saving investigator card data...");
+    let investigator_cards_json = serde_json::to_string_pretty(&investigator_cards).unwrap();
+    fs::write(
+        "assets/cards/investigators/investigators.json",
+        investigator_cards_json,
+    )
+    .unwrap();
+
+    // save each encounter to its own file in encounter folder
+    println!("Saving encounter card data...");
+    for pack in encounter_cards_by_pack.iter() {
+        let pack_code = pack[0]["pack_code"].as_str().unwrap();
+        let pack_json = serde_json::to_string_pretty(&pack).unwrap();
+        fs::write(
+            format!("assets/cards/encounters/{}.json", pack_code),
+            pack_json,
+        )
+        .unwrap();
+    }
 }
 
 // display the help text
 fn display_help() {
     println!("Available commands:");
-    println!("get-all-cards        - Fetch information for all cards");
-    println!("get-card <card_id>   - Fetch information for a card");
-    println!("get-pack <pack_id>   - Fetch information for a pack");
+    println!("update-player-cards   - Fetch and save all player cards");
+    println!("update-all-cards      - Fetch and save player and encounter cards");
 }
